1d67eb367c3da2486b0066912d4f9923731660d5
ZMKZMK <991301630@qq.com>
2017-06-27 17:14:02 +0800

    version1.0

---
+++ application/controllers/index/home.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+
+/*
+ * index 方法默认访问
+ */
+	public function index()
+	{
+		echo "asda";
+		$this->load->view('index/welcome_message');
+	}
+}
